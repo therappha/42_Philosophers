0702e3e6a9cd5bb9f0801628c60f6335d617a362
therappha <therappha@gmail.com>
2025-05-04 21:42:17 +0100

    added checkers for time_to_sleep

--- srcs/sleep.c
+++ srcs/sleep.c
@@ -6,7 +6,7 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 17:13:47 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/05/03 20:38:00 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/05/04 21:40:05 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,24 @@
 
 int	sleeping(t_philo *philo)
 {
+	int	i;
+
+	long long	start_time;
+
+	i = 0;
 	if (ft_get_int(&philo->table->table_mtx, &philo->table->end_simulation))
 		return (0);
 	write_state(philo, SLEEPING);
-	usleep(philo->table->time_to_sleep * 1000);
+
+	start_time = get_time();
+	while (!ft_get_int(&philo->table->table_mtx, &philo->table->end_simulation))
+	{
+		usleep(1000);
+		if (get_time() - start_time >= philo->table->time_to_sleep)
+			break;
+		i++;
+	}
+	if (ft_get_int(&philo->table->table_mtx, &philo->table->end_simulation))
+		return (0);
 	return (1);
 }
